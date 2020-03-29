36f963a7077d8f565ba12145d914be7c767b895d
Rafael Garcia Garcia <37715000+Ralph2g@users.noreply.github.com>
2020-03-28 21:41:53 -0600

    Se modifica y vectoriza la función de el costo de la función

--- Linear regresion(week 1- 2)/Exercices/exercises-1/gradientDescentMulti.m
+++ Linear regresion(week 1- 2)/Exercices/exercises-1/gradientDescentMulti.m
@@ -6,9 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
-
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
@@ -16,22 +14,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    theta = theta - alpha*((X*theta - y)'*X)/(m)'; 
     % ============================================================
-
     % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
-
+    %J_history(iter) = 
 end
-
+disp(theta)
+disp("--")
+disp(computeCostMulti(X, y, theta));
 end

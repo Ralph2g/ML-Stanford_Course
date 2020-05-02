a8e30d6815d0e77ef2b7275a28a4ffbaafc6405a
Rafael Garcia Garcia <37715000+Ralph2g@users.noreply.github.com>
2020-05-01 21:59:13 -0500

    Se termina la primera parte de los ejercicios

--- 7.- Support_Vector_Machine/machine-learning-ex6/ex6/dataset3Params.m
+++ 7.- Support_Vector_Machine/machine-learning-ex6/ex6/dataset3Params.m
@@ -8,9 +8,10 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
-
+C = 0;
+sigma = 0;
+C_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_list = [0.01 0.03 0.1 0.3 1 3 10 30];
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -21,14 +22,21 @@ sigma = 0.3;
 %
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
-%
+error = zeros(length(C_list),length(sigma_list));
+for i = 1:length(C_list)
+    for j =1 :length(sigma_list)
+        model = svmTrain(X,y,C_list(i),@(x1, x2) gaussianKernel(x1, x2, sigma_list(j)));
+        predictions = svmPredict(model, Xval);
+        error(i,j) = mean(double(predictions ~= yval));
+    end 
+end
 
+[~,I] = min(error(:));
 
-
-
-
-
-
+[I_row, I_col] = ind2sub(size(error),I);
+C = C_list(I_row);
+sigma =sigma_list(I_col);
+disp(error)
 % =========================================================================
 
 end
